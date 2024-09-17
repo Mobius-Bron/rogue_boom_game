a5a39b60d83e7652a033e969b980822e4859fd7e
Crane-cell <yzy3223496696@foxmail.com>
2024-09-17 21:12:46 +0800

    更新了角色行走动画

--- player/player.gd
+++ player/player.gd
@@ -4,12 +4,40 @@ extends CharacterBody2D
 const SPEED = 200.0
 
 
-func _physics_process(delta):
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+@export var max_health:TextureProgressBar
+@export var speed=400
+var screen_size
+var current_health
 
-	move_and_slide()
+
+func _ready():
+	screen_size=get_viewport_rect().size
+
+
+func _physics_process(delta):
+	var velocity=Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity.y -=1
+	if Input.is_action_pressed("move_down"):
+		velocity.y +=1		
+	if Input.is_action_pressed("move_left"):
+		velocity.x -=1
+	if Input.is_action_pressed("move_right"):
+		velocity.x +=1
+		
+	if velocity.length()>0:
+		velocity=velocity.normalized()*speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.play()
+	
+	position +=velocity*delta
+	position =position.clamp(Vector2.ZERO,screen_size)
+	
+
+	if velocity.x !=0 or velocity.y !=0:
+		$AnimatedSprite2D.animation="walk"	
+		$AnimatedSprite2D.flip_v=false
+		$AnimatedSprite2D.flip_h=velocity.x >0
+	elif velocity.x ==0 and velocity.y ==0:
+		$AnimatedSprite2D.animation="stay"
